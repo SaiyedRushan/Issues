a55cd32a957029a7237d0f637d36a269de26b5e9
SaiyedRushan <rushan52@gmail.com>
2024-09-02 16:23:19 -0400

    init react project with vite, ts, tailwindcss, shadcnui

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <div>row with buttons</div>
+      <div>list all issues</div>
+    </>
+  )
+}
+
+export default App
